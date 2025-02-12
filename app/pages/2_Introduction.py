6e02360a497fd5ebce835db6ef321278d8273b80
voigtfr <frederik.voigt@uniklinik-freiburg.de>
2025-02-12 13:08:20 +0100

    Fixed typo in filename

--- app/pages/2_Introduction.py
+++ app/pages/2_Introduction.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title=f"{st.session_state.page_title} - Introduction",
-    page_icon=st.session_state.page_icon
+    page_icon=st.session_state.page_icon,
 )
 
 
@@ -12,26 +12,36 @@ st.header("Introduction")
 st.subheader("Virtualization")
 
 with st.expander("Terminology"):
-    st.markdown("""
+    st.markdown(
+        """
     - **Operating Systen (OS)**: Operating Systems
         - GNU/Linux
         - Windows
     - **Kernel**: Core of an OS mediating between applications/users and hardware.
     - **Host**: Computer used to virtualize
     - **Guest**: Virtual machine
-    """)
+    """
+    )
 
 with st.expander("Overview"):
-    st.markdown("""
+    st.markdown(
+        """
     - **Virtualization** is a process, which allows a computer to share its hardware ressources with seperated environments
     - **Virtual Machines** are software defined computers, which can have seperate operating systems and computing ressource (CPU Cores, Memory, ...) 
-    """)
-    st.image("app/static/hypervisor_types.drawio.svg", caption="Type 1 Hypervisor", use_container_width=True)
-    st.markdown("""
+    """
+    )
+    st.image(
+        "app/static/hypervisor_types.drawio.svg",
+        caption="Type 1 Hypervisor",
+        use_container_width=True,
+    )
+    st.markdown(
+        """
     - The **hypervisor manages** virtual machines and most importantly their clear seperation on a hardware level
         - Type 1 Hypervisor: Directly installed on hardware instead of the OS
         - Type 2 Hypervisor: Installed on OS
-    """)
+    """
+    )
 
 st.subheader("Containerization")
 
@@ -44,20 +54,31 @@ with st.expander("OS Level Virtualization"):
 st.subheader("Dev Containers")
 
 with st.expander("Architecture"):
-    st.text("CONTENT")
-    pass
+    st.image(
+        "app/static/architecture_devcontainers.png",
+        caption="VSCode Architecture",
+        use_container_width=True,
+    )
 
 with st.expander("Advantages"):
-    st.markdown("""
+    st.markdown(
+        """
     - Clean capsulation of concerns and environments
     - Easy to setup
     - Documentation of installed dependencies and there respective versions
-    """)
+    """
+    )
 
 column_one, _, column_two = st.columns([3, 5, 2])
 
 with column_one:
-    st.page_link("pages/1_Prerequisites.py", label="Previous: Prerequisites", use_container_width=True)
+    st.page_link(
+        "pages/1_Prerequisites.py",
+        label="Previous: Prerequisites",
+        use_container_width=True,
+    )
 
 with column_two:
-    st.page_link("pages/3_Workshop.py", label="Next: Workshop", use_container_width=True)
+    st.page_link(
+        "pages/3_Workshop.py", label="Next: Workshop", use_container_width=True
+    )
